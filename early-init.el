5a0a187a1db78a119c51992338b3edd7f819794a
Miles Starkenburg <milesstarkenburg@gmail.com>
2024-12-02 13:33:32 -0800

    Config is elaborated

--- early-init.el
+++ early-init.el
@@ -10,5 +10,28 @@
 (setq default-frame-alist
       (append
        (list
-        `(font . ,(concat c/monospace-font "-" (number-to-string c/monospace-font-size))))
+        `(font . ,(concat c/monospace-font "-" (number-to-string c/monospace-font-size)))
+        '(internal-border-width . 0)
+        '(undecorated-round . t)
+        '(left-fringe . 16)
+        '(right-fringe . 16)
+        '(menu-bar-lines . 0)
+        '(tool-bar-lines . 0)
+        '(vertical-scroll-bars))
        default-frame-alist))
+
+  (push '(menu-bar-lines . 0)   default-frame-alist)
+  (push '(tool-bar-lines . 0)   default-frame-alist)
+  (push '(vertical-scroll-bars) default-frame-alist)
+
+(setq frame-inhibit-implied-resize t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(tab-bar-mode 1)
+
+;; Start in fullscreen
+(push '(fullscreen . maximized) default-frame-alist)
+
+

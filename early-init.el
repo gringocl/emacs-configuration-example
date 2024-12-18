4f34e03a6c37b67ea710ac6b3b032d46492f182a
Miles Starkenburg <milesstarkenburg@gmail.com>
2024-12-18 00:50:23 -0800

    Early init is elaborated

--- early-init.el
+++ early-init.el
@@ -1,4 +1,14 @@
-;;; early-init.el -*- lexical-binding: t; -*-
+;;; early-init.el -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(defvar c/gc-cons-threshold (* 16 1024 1024)
+  "The value of `gc-cons-threshold' after Emacs startup.")
+
+(setq gc-cons-threshold most-positive-fixnum)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold c/gc-cons-threshold)))
+
 (defvar c/early-init-el-start-time (current-time) "Time when early-init.el was started")
 (defvar c/section-split-time c/early-init-el-start-time)
 
@@ -23,10 +33,45 @@
 
 (c/start-section "early" "Start")
 
-;; Disable the built-in package manager because Elpaca is used instead
-(setq package-enable-at-startup nil)
+;;; Misc
+(set-language-environment "UTF-8")
+;; Set-language-environment sets default-input-method, which is unwanted.
+(setq default-input-method nil)
+
+;; Without this, Emacs will try to resize itself to a specific column size
+(setq frame-inhibit-implied-resize t)
+
+;; Remove "For information about GNU Emacs..." message at startup
+(advice-add #'display-startup-echo-area-message :override #'ignore)
+
+;; Shave seconds off startup time by starting the scratch buffer in
+;; `fundamental-mode'
+(setq initial-major-mode 'fundamental-mode
+      initial-scratch-message nil)
+
+(defvar c/frame-title-format "%b – Emacs"
+  "Template for displaying the title bar of visible and iconified frame.")
+
+(setq frame-title-format c/frame-title-format
+      icon-title-format c/frame-title-format)
 
 (c/start-section "early" "UI Mode Customization")
+
+;; Disable startup screens and messages
+(setq inhibit-splash-screen t)
+
+;; Disabled initial modes
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(blink-cursor-mode -1)
+
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
+
+;; Enable Tab Bars
+(tab-bar-mode 1)
 
 (setq c/monospace-font "Roboto Mono"
       c/monospace-font-size 15
@@ -39,21 +84,18 @@
        (list
         `(font . ,(concat c/monospace-font "-" (number-to-string c/monospace-font-size)))
         `(font . ,(concat c/variable-pitch-font "-" (number-to-string c/variable-pitch-font-size)))
+        '(menu-bar-lines . 0)
+        '(tool-bar-lines . 0)
         '(internal-border-width . 0)
         '(undecorated-round . t)
         '(left-fringe . 16)
         '(right-fringe . 16)
-        '(menu-bar-lines . 0)
-        '(tool-bar-lines . 0)
         '(vertical-scroll-bars)
+        '(horizontal-scroll-bars)
         '(fullscreen . maximized))
        default-frame-alist))
 
-(setq frame-inhibit-implied-resize t)
-
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(tab-bar-mode 1)
+;; Disable the built-in package manager because Elpaca is used instead
+(setq package-enable-at-startup nil)
 
 (c/start-section "void" "Between early-init and init")

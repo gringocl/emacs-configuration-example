2dd26e9129474eba2eed97261c9f5daaf74aeb0c
Miles Starkenburg <milesstarkenburg@gmail.com>
2025-09-18 19:16:46 -0700

    Elpaca version is increased from 0.9 to 0.11

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (c/start-section "init" "elpaca")
 
 ;; Install Elpaca
-(defvar elpaca-installer-version 0.9)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -15,18 +15,14 @@
                               :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
-
-(c/start-section "init" "elpaca-let")
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
        (default-directory repo))
-       (c/start-section "init" "elpaca-repo")
   (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (c/start-section "init" "elpaca-build-repo")
   (unless (file-exists-p repo)
     (make-directory repo t)
-    (when (< emacs-major-version 28) (require 'subr-x))
+    (when (<= emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
         (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
                   ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
@@ -43,13 +39,13 @@
             (progn (message "%s" (buffer-string)) (kill-buffer buffer))
           (error "%s" (with-current-buffer buffer (buffer-string))))
       ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (c/start-section "init" "elpaca-autoloads")
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
+
 (c/start-section "init" "elpaca-use-package")
 
 (elpaca elpaca-use-package
